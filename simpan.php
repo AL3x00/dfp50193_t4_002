8aefcdb632f51b4ae0eb52c98a37dbd35e8e0fc7
AL3x00 <alexsaw2000@hotmail.com>
2021-12-09 10:27:55 +0800

    text 2

--- simpan.php
+++ simpan.php
@@ -1,6 +1,6 @@
 <?php
 $nama = $_POST['nama'];
 $myfile = fopen("intro.txt", "a") or die("Unable to open file!");
-fwrite($myfile, $nama);
+fwrite($myfile, $nama . "\n");
 fclose($myfile);
 header('location:index.php');
